62d48e429dbea0a8f3201acc18744ff8fb7fefe8
xingrong123 <teyxingrong@gmail.com>
2021-05-18 11:32:20 +0800

    add loading of sudoku puzzle

--- src/index.js
+++ src/index.js
@@ -15,7 +15,9 @@ class Board extends React.Component {
   renderSquare(i) {
     var style = {background: "white"};
     if (this.props.selectedSquare === i) {
-      style = {background: "yellow"}
+      style = {background: "Yellow"}
+    } else if (this.props.puzzleIndex) {
+      style = (this.props.puzzleIndex.includes(i)) ? {background: "LightGray"} : style;
     }
     return (
       <Square 
@@ -68,13 +70,34 @@ class Board extends React.Component {
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      squares: Array(81).fill(null), 
-      selectedSquare: null,
+
+    if (!this.props.puzzle) {
+      this.state = {
+        squares: Array(81).fill(null), 
+        selectedSquare: null,
+        puzzleIndex: null,
+      }
+    } else {
+      var puzzleIndex = [];
+      for (var i = 0; i < 81; i++) {
+        if (this.props.puzzle[i]) {
+          puzzleIndex = puzzleIndex.concat(i);
+        }
+      }
+      this.state = {
+        squares: this.props.puzzle, 
+        selectedSquare: null, 
+        puzzleIndex: puzzleIndex, 
+      };
     }
   }
 
   handleClick(i) {
+    if (this.state.puzzleIndex) {
+      if (this.state.puzzleIndex.includes(i)) {
+        return;
+      }
+    }
     this.setState({selectedSquare: i})
   }
 
@@ -95,15 +118,16 @@ class Game extends React.Component {
     this.setState({squares: squares});
   }
 
-
   render() {
-    const squares = this.state.squares;
+    const squares = this.state.squares.slice();
+    const puzzleIndex = this.state.puzzleIndex ? this.state.puzzleIndex.slice() : null;
     return (
       <div className="game">
         <div className="game-board">
           <Board 
             squares={squares} 
             selectedSquare={this.state.selectedSquare}
+            puzzleIndex={puzzleIndex}
             onClick={(i) => this.handleClick(i)} 
             onContextMenu={(i, e) => this.handleContextMenu(i, e)}
             onKeyPress={(num) => this.handleKeyPress(num)}
@@ -120,7 +144,9 @@ class Game extends React.Component {
 
 // ========================================
 
+const puzzle = [null,2,null,8,null,null,null,4,3,null,5,null,3,null,9,null,null,null,4,null,null,null,null,null,1,9,null,6,8,null,1,3,2,null,null,null,7,3,null,null,9,8,null,6,null,null,1,9,null,6,4,null,3,null,3,4,null,null,null,null,7,8,6,1,null,7,null,8,null,null,5,null,null,null,8,4,null,7,null,null,9]
+
 ReactDOM.render(
-  <Game />,
+  <Game puzzle={puzzle}/>,
   document.getElementById('root')
 );
