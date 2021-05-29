43209eb744abe7b3ac3007815f9848f559c8f5fa
xingrong123 <teyxingrong@gmail.com>
2021-05-29 13:18:33 +0800

    update postgres database

--- src/server/db/sudoku.sql
+++ src/server/db/sudoku.sql
@@ -10,11 +10,12 @@ for line in lines:
     ans = ans + "  (\'{" + (",".join(line[i:i + 1] for i in range(0, len(line), 1)).replace("0", "null")) + "}\'),\n"
 print(ans)
 */
-
+/*
 DROP DATABASE IF EXISTS sudoku_db;
 CREATE DATABASE sudoku_db;
 \c sudoku_db;
--- DROP TABLE IF EXISTS sudoku_puzzles;
+*/
+DROP TABLE IF EXISTS sudoku_puzzles;
 CREATE TABLE sudoku_puzzles (
   id SERIAL PRIMARY KEY,
   puzzle INTEGER[81] NOT NULL UNIQUE
@@ -22,4 +23,5 @@ CREATE TABLE sudoku_puzzles (
 INSERT INTO sudoku_puzzles(puzzle) VALUES 
   ('{9,null,null,null,null,null,null,null,1,null,null,7,8,3,1,6,4,9,6,1,null,5,4,null,8,null,null,null,null,null,1,null,null,null,null,6,7,4,5,null,9,6,2,null,null,null,null,6,null,null,4,7,5,null,3,7,null,4,null,null,9,null,2,4,null,null,null,6,null,null,8,5,5,null,1,null,null,8,null,null,null}'),
   ('{null,null,9,null,null,1,null,null,null,null,null,null,null,4,9,null,null,null,3,null,null,null,null,null,1,8,null,4,null,7,5,null,8,null,null,6,null,8,null,null,null,2,9,null,7,null,1,null,null,null,null,null,5,null,null,6,5,4,2,null,3,9,null,7,null,1,6,null,null,null,null,5,null,null,null,9,null,null,null,6,null}'),
+  ('{null,9,2,null,1,null,null,null,3,7,null,null,null,4,3,9,null,null,6,null,1,7,null,2,null,null,null,9,5,null,null,null,null,null,2,null,3,null,4,null,5,null,1,null,9,null,2,null,null,null,null,null,4,5,null,null,null,8,null,6,5,null,4,null,null,6,4,3,null,null,null,1,5,null,null,null,2,null,8,6,null}'),
   ('{null,null,null,null,null,null,null,4,null,3,null,null,null,null,1,7,2,null,null,5,null,null,null,null,8,null,null,null,null,null,null,null,2,null,null,null,null,null,null,5,6,null,null,null,null,6,null,3,null,null,7,2,null,4,1,3,null,null,8,null,null,7,null,5,9,null,null,3,null,1,null,2,null,null,4,2,null,9,null,null,8}');
