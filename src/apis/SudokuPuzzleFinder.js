3b506a2b85448049f1f5d64723e4f39fb8dcac0b
xingrong123 <teyxingrong@gmail.com>
2021-05-29 13:41:27 +0800

    set baseURL of heroku to empty

--- src/apis/SudokuPuzzleFinder.js
+++ src/apis/SudokuPuzzleFinder.js
@@ -3,5 +3,5 @@ import axios from "axios";
 let development = process.env.NODE_ENV !== 'production';
 
 export default axios.create({
-  baseURL: development ? 'http://localhost:3002' : 'postgres://nepiugirwevemu:238854ecfd36e4965de8e9b85e21febae9a73dff1f3aabd1ea503cff924bc074@ec2-3-215-57-87.compute-1.amazonaws.com:5432/d59vo4mskjgrea'
+  baseURL: development ? 'http://localhost:3002' : ''
 });
