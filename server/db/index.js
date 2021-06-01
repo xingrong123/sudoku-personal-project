47e602854bd6a74365518fa17bdadf9eb9abfbfe
xingrong123 <teyxingrong@gmail.com>
2021-06-01 13:47:39 +0800

    enable ssl for postgres connection on heroku

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg')
+const dbUrl = process.env.NODE_ENV !== 'production' ? process.env.DATABASE_URL : DATABASE_URL + '?ssl=true'
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: dbUrl,
 })
 
 module.exports = {
