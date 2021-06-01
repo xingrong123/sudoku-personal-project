7097a77169d22065e90c3a3903885cab9067b6f8
xingrong123 <teyxingrong@gmail.com>
2021-06-01 15:19:35 +0800

    serve static files from react app

--- server/server.js
+++ server/server.js
@@ -1,24 +1,28 @@
 require('dotenv').config();
 const express = require("express")
+const path = require('path');
 const cors = require("cors");
 const db = require("./db")
 const app = express()
 
 app.use(cors());
 
+// app.use((req, res, next) => {
+//   console.log("yeah our middle ware");
+//   next();
+// })
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 // Middleware to reconize the incoming Request Object as a JSON Object.
 app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log("yeah our middle ware");
-  next();
-})
 
 app.get("/", async (req, res) => {
   try {
     const results = await db.query("SELECT * FROM sudoku_puzzles");
 
-    res.status(200).json({
+    res.json({
       status: "success",
       data: results.rows
     });
@@ -26,6 +30,12 @@ app.get("/", async (req, res) => {
     console.log(err)
   }
 })
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
