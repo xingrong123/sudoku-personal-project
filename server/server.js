51682017239b7b00ab16cc65489d7b85bfe1f7f0
xingrong123 <teyxingrong@gmail.com>
2021-06-02 12:38:34 +0800

    change directory traversal for app get

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.get("/api/puzzles", async (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'../client/build/index.html'));
 });
 
 const port = process.env.PORT || 3002;
